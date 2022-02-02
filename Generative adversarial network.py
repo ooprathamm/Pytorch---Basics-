f33ce307d42fe10ee994e8ab5d3be6f3d98a4642
rajputchauhan1 <prathamchauhan2001@gmail.com>
2022-02-02 13:45:15 +0530

    Added GAN

---
+++ Generative adversarial network.py
@@ -0,0 +1,207 @@
+import os
+from sklearn.metrics import recall_score
+import torch
+import torchvision
+from torch.utils.data import DataLoader,random_split
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as T
+from torchvision.utils import make_grid
+import matplotlib
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.utils import save_image
+
+
+data_dir='./animefacedataset'
+image_size=64
+batch_size=128
+stats=(0.5,0.5,0.5),(0.5,0.5,0.5)
+train_ds=ImageFolder(data_dir,transform=T.Compose([
+    T.Resize(image_size),
+    T.CenterCrop(image_size),
+    T.ToTensor(),
+    T.Normalize(*stats)]))
+
+train_dl=DataLoader(train_ds,batch_size,shuffle=True,num_workers=4,pin_memory=True)
+
+def denormalize(img_tensors):
+    return img_tensors*stats[1][0] + stats[0][0]
+
+def show_images(images,nmax=64):
+    fig,ax=plt.subplots(figsize=(8,8))
+    ax.set_xticks([]);ax.set_yticks([])
+    ax.imshow(make_grid(denormalize(images.detach()[:nmax]),nrow=8).permute(1,2,0))
+    plt.show()
+
+def show_batch(dl,nmax=64):
+    for images,_ in dl:
+        show_images(images,nmax)
+        break
+
+
+def get_default_device():
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')    
+device=get_default_device()
+def to_device(data,device):
+    if isinstance(data,(list,tuple)):
+        return [to_device(x,device) for x in data]
+    return data.to(device,non_blocking=True)
+class DeviceDataLoader():
+    def __init__(self,dl,device):
+        self.dl=dl
+        self.device=device
+    def __iter__(self):
+        for b in self.dl:
+            yield to_device(b,self.device)
+    def __len__(self):
+        return len(self.dl)            
+
+train_dl=DeviceDataLoader(train_dl,device)        
+
+discriminator = nn.Sequential(
+   
+
+    nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(64),
+    nn.LeakyReLU(0.2, inplace=True),
+
+    nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(128),
+    nn.LeakyReLU(0.2, inplace=True),
+
+    nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(256),
+    nn.LeakyReLU(0.2, inplace=True),
+
+    nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(512),
+    nn.LeakyReLU(0.2, inplace=True),
+
+    nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+
+    nn.Flatten(),
+    nn.Sigmoid())
+
+discriminator=to_device(discriminator,device)
+
+latent_size=128
+
+generator = nn.Sequential(
+
+    nn.ConvTranspose2d(latent_size, 512, kernel_size=4, stride=1, padding=0, bias=False),
+    nn.BatchNorm2d(512),
+    nn.ReLU(True),
+
+    nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(256),
+    nn.ReLU(True),
+
+    nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(128),
+    nn.ReLU(True),
+
+    nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(64),
+    nn.ReLU(True),
+
+    nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.Tanh()
+)
+
+xb=torch.randn(batch_size,latent_size,1,1,)
+fake_images=generator(xb)
+show_images(fake_images)
+
+generator=to_device(generator,device)
+
+def train_discriminator(real_images,opt_d):
+    opt_d.zero_grad()
+
+    real_preds=discriminator(real_images)
+    real_targets=torch.ones(real_images.size(0),1,device=device)
+    real_loss=F.binary_cross_entropy(real_preds,real_targets)
+    real_score=torch.mean(real_loss).item()
+    
+    latent=torch.zeros(batch_size,latent_size,1,1,device=device)
+    fake_images=generator(latent)
+
+    fake_targets=torch.zeros(fake_images.size(0),1,device=device)
+    fake_preds=discriminator(fake_images)
+    fake_loss=F.binary_cross_entropy(fake_preds,fake_targets)
+    fake_score=torch.mean(fake_preds).item()
+    
+    loss=real_loss+fake_loss
+    loss.backward()
+    opt_d.step()
+    return loss.item(),real_score,fake_score
+
+def train_generator(opt_g):
+    opt_g.zero_grad()
+
+    latent=torch.randn(batch_size,latent_size,1,1,device=device)
+    fake_images=generator(latent)
+
+    preds=discriminator(fake_images)
+    targets=torch.ones(batch_size,1,device=device)
+    loss=F.binary_cross_entropy(preds,targets)
+
+    loss.backward()
+    opt_g.step()
+
+    return loss.item()
+
+dir='generated'
+os.makedirs(dir,exist_ok=True)
+
+def save_samples(index,latent_tensors,show=True):
+    fake_images=generator(latent_tensors)
+    fake_name="image_{}.png".format(index)
+    save_image(denormalize(fake_images),os.path.join(dir,fake_name),nrow=8)
+    print('Saving', fake_name)
+    if show:
+        fig, ax = plt.subplots(figsize=(8, 8))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(fake_images.cpu().detach(), nrow=8).permute(1, 2, 0))
+        plt.show()
+        
+fixed_latent = torch.randn(64, latent_size, 1, 1, device=device)
+
+def fit(epochs, lr, start_idx=1):
+    torch.cuda.empty_cache()
+    
+  
+    losses_g = []
+    losses_d = []
+    real_scores = []
+    fake_scores = []
+    
+ 
+    opt_d = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=(0.5, 0.999))
+    opt_g = torch.optim.Adam(generator.parameters(), lr=lr, betas=(0.5, 0.999))
+    
+    for epoch in range(epochs):
+        for real_images, _ in train_dl:
+           
+            loss_d, real_score, fake_score = train_discriminator(real_images, opt_d)
+            loss_g = train_generator(opt_g)
+            
+       
+        losses_g.append(loss_g)
+        losses_d.append(loss_d)
+        real_scores.append(real_score)
+        fake_scores.append(fake_score)
+  
+        print("Epoch [{}/{}], loss_g: {:.4f}, loss_d: {:.4f}, real_score: {:.4f}, fake_score: {:.4f}".format(
+            epoch+1, epochs, loss_g, loss_d, real_score, fake_score))
+    
+        save_samples(epoch+start_idx, fixed_latent, show=False)
+    
+    return losses_g, losses_d, real_scores, fake_scores
+
+lr = 0.0002
+epochs = 25    
+history = fit(epochs, lr)
